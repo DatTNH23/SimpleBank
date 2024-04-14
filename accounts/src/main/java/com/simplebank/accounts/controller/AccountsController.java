a74911a19a1637c70c3a07777330fc988c9951fb
Dat <tilamhanh123@gmail.com>
2024-04-14 17:02:14 +0200

    Perform input data validations inside accounts microservice

--- accounts/src/main/java/com/simplebank/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/simplebank/accounts/controller/AccountsController.java
@@ -10,18 +10,20 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
+@Validated
 public class AccountsController {
     private AccountsService accountsService;
 
 
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto){
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto){
         accountsService.createAccount(customerDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
--- accounts/src/main/java/com/simplebank/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/simplebank/accounts/dto/CustomerDto.java
@@ -1,10 +1,14 @@
 package com.simplebank.accounts.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
+    @NotEmpty(message = "Name can not be a null or empty")
+    @Size(min = 5, max = 30, message = "The length of the customer name should be between 5 and 30")
     private String name;
+    @NotEmpty
     private String email;
     private String mobileNumber;
     private AccountsDto accountsDto;
