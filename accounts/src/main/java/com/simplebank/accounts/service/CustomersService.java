1f98f1ab0c594dac491434fa86c4b6d2f5925bb1
Dat <tilamhanh123@gmail.com>
2024-08-13 20:21:09 +0200

    Add Feign Client code for Account service

---
+++ accounts/src/main/java/com/simplebank/accounts/service/CustomersService.java
@@ -0,0 +1,12 @@
+package com.simplebank.accounts.service;
+
+import com.simplebank.accounts.dto.CustomerDetailsDto;
+
+public interface CustomersService {
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+}
---
+++ accounts/src/main/java/com/simplebank/accounts/service/impl/CustomersServiceImpl.java
@@ -0,0 +1,55 @@
+package com.simplebank.accounts.service.impl;
+
+import com.simplebank.accounts.dto.AccountsDto;
+import com.simplebank.accounts.dto.CardsDto;
+import com.simplebank.accounts.dto.CustomerDetailsDto;
+import com.simplebank.accounts.dto.LoansDto;
+import com.simplebank.accounts.entity.Accounts;
+import com.simplebank.accounts.entity.Customer;
+import com.simplebank.accounts.exception.ResourceNotFoundException;
+import com.simplebank.accounts.mapper.AccountsMapper;
+import com.simplebank.accounts.mapper.CustomerMapper;
+import com.simplebank.accounts.repository.AccountsRepository;
+import com.simplebank.accounts.repository.CustomerRepository;
+import com.simplebank.accounts.service.CustomersService;
+import com.simplebank.accounts.service.client.CardsFeignClient;
+import com.simplebank.accounts.service.client.LoansFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@AllArgsConstructor
+public class CustomersServiceImpl implements CustomersService {
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+    private CardsFeignClient cardsFeignClient;
+    private LoansFeignClient loansFeignClient;
+
+    /**
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
+        );
+
+        CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
+
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(mobileNumber);
+        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
+        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+
+        return customerDetailsDto;
+
+    }
+}
