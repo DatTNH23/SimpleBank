9f692855d58f8c0f6d8f4dee1469839da04c4a70
Dat <tilamhanh123@gmail.com>
2024-08-19 12:25:55 +0200

    Add custom Filter and adapt docker compose

--- cards/src/main/java/com/simplebank/cards/controller/CardsController.java
+++ cards/src/main/java/com/simplebank/cards/controller/CardsController.java
@@ -16,6 +16,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -34,7 +36,7 @@ import org.springframework.web.bind.annotation.*;
 public class CardsController {
     @Autowired
     private CardsService cardsService;
-
+    private static final Logger logger = LoggerFactory.getLogger(CardsController.class);
 
     private String buildVersion = "1.0";
 
@@ -88,9 +90,12 @@ public class CardsController {
             )
     })
     @GetMapping("/fetch")
-    public ResponseEntity<CardsDto> fetchCardDetails(@RequestParam
+    public ResponseEntity<CardsDto> fetchCardDetails(@RequestHeader("simplebank-correlation-id")
+                                                         String correlationId,
+                                                     @RequestParam
                                                      @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                                                      String mobileNumber) {
+        logger.debug("simpleBank-correlation-id found {}", correlationId);
         CardsDto cardsDto = cardsService.fetchCard(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
     }
--- loans/src/main/java/com/simplebank/loans/controller/LoansController.java
+++ loans/src/main/java/com/simplebank/loans/controller/LoansController.java
@@ -15,6 +15,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -33,6 +35,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Validated
 public class LoansController {
+    private static final Logger logger = LoggerFactory.getLogger(LoansController.class);
     @Autowired
     private LoansService loansService;
 
@@ -88,9 +91,11 @@ public class LoansController {
     }
     )
     @GetMapping("/fetch")
-    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestParam
+    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestHeader("simplebank-correlation-id")
+                                                         String correlationId, @RequestParam
                                                      @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                                                      String mobileNumber) {
+        logger.debug("simpleBank-correlation-id found {}", correlationId);
         LoansDto loansDto = loansService.fetchLoan(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(loansDto);
     }
